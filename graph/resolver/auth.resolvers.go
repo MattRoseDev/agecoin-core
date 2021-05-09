c002a03c46f53fa3902170d4ae59f6bea81adbf5
Matt Rose <mattroseinbox@gmail.com>
2021-05-10 03:55:02 +0430

    feat: initial github.com/99designs/gqlgen

---
+++ graph/resolver/auth.resolvers.go
@@ -0,0 +1,19 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/favecode/agecoin-core/graph/model"
+)
+
+func (r *mutationResolver) Register(ctx context.Context, input model.RegisterInput) (*model.AuthResponse, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) Login(ctx context.Context, input model.LoginInput) (*model.AuthResponse, error) {
+	panic(fmt.Errorf("not implemented"))
+}
