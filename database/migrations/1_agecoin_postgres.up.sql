aa56d370088484997f2efdd666dc343bc5615b30
Mahdi <mahdineer@pm.me>
2021-05-25 12:35:28 +0430

    fix: change field Tables

--- database/migrations/1_agecoin_postgres.up.sql
+++ database/migrations/1_agecoin_postgres.up.sql
@@ -35,9 +35,10 @@ CREATE TABLE "password" (
 
 CREATE TABLE "task" (
 	"id" uuid NOT NULL DEFAULT uuid_generate_v4(),
-	"parent_id" uuid,
 	"user_id" uuid NOT NULL,
 	"title" TEXT NOT NULL,
+	"description" TEXT,
+	"amount" integer, 
 	"created_at" TIMESTAMP NOT NULL DEFAULT (NOW()),
 	"updated_at" TIMESTAMP NOT NULL DEFAULT (NOW()),
 	"deleted_at" TIMESTAMP,
@@ -46,28 +47,14 @@ CREATE TABLE "task" (
   OIDS=FALSE
 );
 
-CREATE TABLE "user_task" (
-	"id" uuid NOT NULL DEFAULT uuid_generate_v4(),
-	"user_id" uuid NOT NULL,
-	"task_id" uuid NOT NULL,
-	"amount" integer NOT NULL,
-	"created_at" TIMESTAMP NOT NULL,
-	"updated_at" TIMESTAMP NOT NULL,
-	"deleted_at" TIMESTAMP,
-	CONSTRAINT "user_task_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
-);
-
-
-
 CREATE TABLE "current_task" (
 	"id" uuid NOT NULL DEFAULT uuid_generate_v4(),
 	"user_id" uuid NOT NULL,
-	"user_task_id" uuid NOT NULL,
-	"amount" integer NOT NULL,
+	"task_id" uuid NOT NULL,
+	"default_amount" integer NOT NULL,
+	"amount" integer,
 	"status" integer NOT NULL,
-	"spent" integer,
+	"description" TEXT,
 	"active" BOOLEAN NOT NULL DEFAULT FALSE,
 	"created_at" TIMESTAMP NOT NULL,
 	"updated_at" TIMESTAMP NOT NULL,
@@ -89,17 +76,12 @@ $$ language 'plpgsql';
 CREATE TRIGGER update_user BEFORE UPDATE ON "user" FOR EACH ROW EXECUTE PROCEDURE  trigger_set_updated_at();
 CREATE TRIGGER update_password BEFORE UPDATE ON "password" FOR EACH ROW EXECUTE PROCEDURE  trigger_set_updated_at();
 CREATE TRIGGER update_task BEFORE UPDATE ON "task" FOR EACH ROW EXECUTE PROCEDURE  trigger_set_updated_at();
-CREATE TRIGGER update_user_task BEFORE UPDATE ON "user_task" FOR EACH ROW EXECUTE PROCEDURE  trigger_set_updated_at();
 CREATE TRIGGER update_current_task BEFORE UPDATE ON "current_task" FOR EACH ROW EXECUTE PROCEDURE  trigger_set_updated_at();
 
 -- Foreign keys
 ALTER TABLE "password" ADD CONSTRAINT "password_fk0" FOREIGN KEY ("user_id") REFERENCES "user"("id");
 
 ALTER TABLE "task" ADD CONSTRAINT "task_fk0" FOREIGN KEY ("user_id") REFERENCES "user"("id");
-ALTER TABLE "task" ADD CONSTRAINT "task_fk1" FOREIGN KEY ("parent_id") REFERENCES "task"("id");
-
-ALTER TABLE "user_task" ADD CONSTRAINT "user_task_fk0" FOREIGN KEY ("user_id") REFERENCES "user"("id");
-ALTER TABLE "user_task" ADD CONSTRAINT "user_task_fk1" FOREIGN KEY ("task_id") REFERENCES "task"("id");
 
 ALTER TABLE "current_task" ADD CONSTRAINT "current_task_fk0" FOREIGN KEY ("user_id") REFERENCES "user"("id");
-ALTER TABLE "current_task" ADD CONSTRAINT "current_task_fk1" FOREIGN KEY ("user_task_id") REFERENCES "user_task"("id");
+ALTER TABLE "current_task" ADD CONSTRAINT "current_task_fk1" FOREIGN KEY ("task_id") REFERENCES "task"("id");
