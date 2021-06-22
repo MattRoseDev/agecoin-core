a5ebdc0cbcbcdfda77ae2c169c71ebd29d8a1f6f
Mahdi <mahdineer@pm.me>
2021-06-23 02:03:38 +0430

    fix: change task_id field to current_task_id for current_task_history table

--- graph/model/currentTaskHistory.go
+++ graph/model/currentTaskHistory.go
@@ -5,12 +5,12 @@ import (
 )
 
 type CurrentTaskHistory struct {
-	tableName struct{}   `sql:"current_task_history"`
-	ID        string     `json:"id"`
-	UserID    string     `json:"userId"`
-	TaskID    string     `json:"taskId"`
-	Type      *string    `json:"type"`
-	CreatedAt time.Time  `json:"createdAt"`
-	UpdatedAt time.Time  `json:"updatedAt"`
-	DeletedAt *time.Time `json:"-" pg:",soft_delete"`
+	tableName     struct{}   `sql:"current_task_history"`
+	ID            string     `json:"id"`
+	UserID        string     `json:"userId"`
+	CurrentTaskID string     `json:"currentTaskId"`
+	Type          string     `json:"type"`
+	CreatedAt     time.Time  `json:"createdAt"`
+	UpdatedAt     time.Time  `json:"updatedAt"`
+	DeletedAt     *time.Time `json:"-" pg:",soft_delete"`
 }
