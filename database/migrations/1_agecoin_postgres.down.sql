4864cb1a391a831cd3e28798904f29a1cd2d6619
Mahdi <mahdineer@pm.me>
2021-06-29 11:51:06 +0430

    refactor: combine task and current_task table

--- database/migrations/1_agecoin_postgres.down.sql
+++ database/migrations/1_agecoin_postgres.down.sql
@@ -2,15 +2,9 @@ ALTER TABLE "password" DROP CONSTRAINT IF EXISTS "password_fk0";
 
 ALTER TABLE "task" DROP CONSTRAINT IF EXISTS "task_fk0";
 
-ALTER TABLE "task" DROP CONSTRAINT IF EXISTS "task_fk1";
+ALTER TABLE "task_history" DROP CONSTRAINT IF EXISTS "task_history_fk0";
 
-ALTER TABLE "current_task" DROP CONSTRAINT IF EXISTS "current_task_fk0";
-
-ALTER TABLE "current_task" DROP CONSTRAINT IF EXISTS "current_task_fk1";
-
-ALTER TABLE "current_task_history" DROP CONSTRAINT IF EXISTS "current_task_history_fk0";
-
-ALTER TABLE "current_task_history" DROP CONSTRAINT IF EXISTS "current_task_history_fk1";
+ALTER TABLE "task_history" DROP CONSTRAINT IF EXISTS "task_history_fk1";
 
 DROP TABLE IF EXISTS "user";
 
@@ -18,10 +12,8 @@ DROP TABLE IF EXISTS "password";
 
 DROP TABLE IF EXISTS "task";
 
-DROP TABLE IF EXISTS "current_task";
-
-DROP TABLE IF EXISTS "current_task_history";
+DROP TABLE IF EXISTS "task_history";
 
 DROP TYPE IF EXISTS "user_roles";
 
-DROP TYPE IF EXISTS "current_task_history_type";
+DROP TYPE IF EXISTS "task_history_type";
